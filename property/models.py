9484b74c2a22ad41f5126e4417e562544c087fe0
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-05-07 07:36:57 +1100

    Add field Flat.liked_by

--- property/models.py
+++ property/models.py
@@ -48,6 +48,7 @@ class Flat(models.Model):
         blank=True,
         db_index=True)
     new_building = models.NullBooleanField('New_building')
+    liked_by = models.ManyToManyField(User, verbose_name='Кто лайкнул', related_name='liked_flats')
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
