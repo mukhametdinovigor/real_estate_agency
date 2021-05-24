1cac5b38184ad507bd2093e755bf0ca63f3d0956
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-05-24 23:30:56 +1100

    Use double quotes

--- property/admin.py
+++ property/admin.py
@@ -5,25 +5,25 @@ from .models import Flat, Complaint, Owner
 
 class OwnerInline(admin.TabularInline):
     model = Owner.flats.through
-    raw_id_fields = ('owner',)
+    raw_id_fields = ("owner",)
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ('town', 'address')
-    readonly_fields = ['created_at']
-    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
-    list_editable = ['new_building']
-    list_filter = ('new_building', 'rooms_number', 'has_balcony')
-    raw_id_fields = ('liked_by',)
+    search_fields = ("town", "address")
+    readonly_fields = ["created_at"]
+    list_display = ("address", "price", "new_building", "construction_year", "town")
+    list_editable = ["new_building"]
+    list_filter = ("new_building", "rooms_number", "has_balcony")
+    raw_id_fields = ("liked_by",)
     inlines = [OwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
-    raw_id_fields = ('complainer', 'complained_flat_about')
+    raw_id_fields = ("complainer", "complained_flat_about")
 
 
 class OwnerAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flats',)
+    raw_id_fields = ("flats",)
 
 
 admin.site.register(Flat, FlatAdmin)
--- property/models.py
+++ property/models.py
@@ -21,14 +21,14 @@ class Flat(models.Model):
         "Район города, где находится квартира",
         max_length=50,
         blank=True,
-        help_text='Чертаново Южное')
+        help_text="Чертаново Южное")
     address = models.TextField(
         "Адрес квартиры",
-        help_text='ул. Подольских курсантов д.5 кв.4')
+        help_text="ул. Подольских курсантов д.5 кв.4")
     floor = models.CharField(
         "Этаж",
         max_length=3,
-        help_text='Первый этаж, последний этаж, пятый этаж')
+        help_text="Первый этаж, последний этаж, пятый этаж")
 
     rooms_number = models.IntegerField(
         "Количество комнат в квартире",
@@ -46,11 +46,11 @@ class Flat(models.Model):
         null=True,
         blank=True,
         db_index=True)
-    new_building = models.NullBooleanField('Новое здание', blank=True, db_index=True)
+    new_building = models.NullBooleanField("Новое здание", blank=True, db_index=True)
     liked_by = models.ManyToManyField(
         User,
-        verbose_name='Кто лайкнул',
-        related_name='liked_flats',
+        verbose_name="Кто лайкнул",
+        related_name="liked_flats",
         blank=True,
         db_index=True)
 
@@ -59,25 +59,25 @@ class Flat(models.Model):
 
 
 class Complaint(models.Model):
-    complainer = models.ForeignKey(User, verbose_name='Кто жаловался', on_delete=models.CASCADE)
+    complainer = models.ForeignKey(User, verbose_name="Кто жаловался", on_delete=models.CASCADE)
     complained_flat_about = models.ForeignKey(
         Flat,
-        verbose_name='Квартира на которую жаловались',
+        verbose_name="Квартира на которую жаловались",
         on_delete=models.CASCADE)
-    complaint_text = models.TextField('Текст жалобы')
+    complaint_text = models.TextField("Текст жалобы")
 
     def __str__(self):
         return self.complainer.username
 
 
 class Owner(models.Model):
-    owner_name = models.CharField('ФИО владельца', max_length=200, db_index=True)
-    owner_phonenumber = models.CharField('Номер владельца', max_length=20, db_index=True)
-    owner_pure_phone = PhoneNumberField(verbose_name='Нормализованный номер владельца', blank=True, db_index=True)
+    owner_name = models.CharField("ФИО владельца", max_length=200, db_index=True)
+    owner_phonenumber = models.CharField("Номер владельца", max_length=20, db_index=True)
+    owner_pure_phone = PhoneNumberField(verbose_name="Нормализованный номер владельца", blank=True, db_index=True)
     flats = models.ManyToManyField(
         Flat,
-        verbose_name='Квартиры в собственности',
-        related_name='flat_owners',
+        verbose_name="Квартиры в собственности",
+        related_name="flat_owners",
         db_index=True)
 
     def __str__(self):
